bb9910b6ad54b81b662dcaaae33a8fe558e423e0
Biju Kunjummen <biju.kunjummen@gmail.com>
2012-04-20 19:43:23 -0400

    Upgraded hibernate libs

--- src/main/java/org/bk/lmt/web/GtdContextController.java
+++ src/main/java/org/bk/lmt/web/GtdContextController.java
@@ -1,13 +1,17 @@
 package org.bk.lmt.web;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
+import org.bk.lmt.domain.GtdContext;
 import org.bk.lmt.service.GtdContextService;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -18,14 +22,37 @@ public class GtdContextController {
 	@Resource private GtdContextService gtdContextService;	
 	
 	@RequestMapping(produces="text/html")
-	public String list(@RequestParam(defaultValue="1",value="page", required=false) Integer page, 
-				@RequestParam(defaultValue="10", value="size", required=false) Integer size, Model model){
+	public String list(@RequestParam(defaultValue="1",value="page", required=false) Integer page, @RequestParam(defaultValue="10", value="size", required=false) Integer size, Model model){
     	Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     	String userName = ((User)principal).getUsername();
     	model.addAttribute("contexts", this.gtdContextService.findContextsByGtdUserName(userName, page == null ? 0 : (page.intValue() - 1) * size, size));
-        float nrOfPages = this.gtdContextService.countContextsByUserName(userName);
+        float nrOfPages = this.gtdContextService.countContextsByUserName(userName)/size;
         model.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
     	
 		return "contexts/list";
+	}
+
+	@RequestMapping(method=RequestMethod.POST, produces="text/html")
+	public String create(@Valid GtdContext context, BindingResult bindingResult, Model model){
+    	Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    	String userName = ((User)principal).getUsername();
+		
+		if (bindingResult.hasErrors()){
+			populateEditForm(model, context);
+			return "contexts/create";
+		}
+		model.asMap().clear();
+		this.gtdContextService.persistForUser(context, userName);
+		return "redirect:/students/list";
+	}
+
+	@RequestMapping(params="form", produces="text/html")
+	public String createForm(Model model){
+		populateEditForm(model, new GtdContext());
+		return "contexts/create";
+	}
+	
+	private void populateEditForm(Model model, GtdContext context){
+		model.addAttribute("context", context);
 	}
 }
